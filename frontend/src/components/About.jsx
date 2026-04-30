9311585324e68328693ad6568e6f89be0ca6c5eb
enjoythedivision <marialapps@gmail.com>
2026-04-30 21:06:03 +0300

    add About component with project description; update package.json and package-lock.json for react-router-dom

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <>
+      <p>
+        This project is a simple library interface built with React. It
+        allows
+        users to browse, search, and filter books. The
+        goal is not to replicate a full library system (for now!), but to explore core
+        frontend/react concepts like component-based architecture, state management, and
+        dynamic rendering. Since I like taking care of the visual aspect of my projects as well, I implemented a clean and minimal interface with vanilla CSS. 
+        This project is part of an ongoing process of
+        learning and refining modern web development practices, with future
+        plans to connect it to a backend and evolve it into a fully functional
+        application.
+      </p>
+    </>
+  );
+}
