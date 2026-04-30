d32c8122800c9b67acbb2522dcd49a7bd78924a7
enjoythedivision <marialapps@gmail.com>
2026-04-30 21:41:33 +0300

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,16 +2,16 @@ export default function About() {
   return (
     <>
       <p>
-        This project is a simple library interface built with React. It
-        allows
-        users to browse, search, and filter books. The
-        goal is not to replicate a full library system (for now!), but to explore core
+        This project is a simple library interface built with React. <br /><br />
+        It allows users to browse, search, and filter books. <br /><br />
+        The goal is not to replicate a full library system (for now!), but to explore core
         frontend/react concepts like component-based architecture, state management, and
-        dynamic rendering. Since I like taking care of the visual aspect of my projects as well, I implemented a clean and minimal interface with vanilla CSS. 
-        This project is part of an ongoing process of
-        learning and refining modern web development practices, with future
-        plans to connect it to a backend and evolve it into a fully functional
-        application.
+        dynamic rendering. <br /><br />
+        Since I like taking care of the visual aspect of my projects as well, I implemented a clean and minimal interface with vanilla CSS, where each stylesheet lives next to its component.
+        A great alternative option would be SCSS, but due to the small size of the project, I found it redundant. <br /><br />
+        This project is part of an ongoing process of learning and refining web development practices, with future
+        plans to connect it to a backend and evolve it into a fully functional application. <br /><br />
+        Thanks for stopping by!
       </p>
     </>
   );
--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,22 +1,19 @@
-import "./BookCard.css"
+import "./BookCard.css";
 
-export default function BookCard({book}) {
+export default function BookCard({ book }) {
   return (
     <div className="card">
-        <div className="cover">
+      <div className="cover">
+        <img src={book.photo} alt={book.title} />
+      </div>
 
-        </div>
-        <div className="metadata">
-            <h2 className="title">
-                {book.title}
-            </h2>
-            <h3 className="author">
-                {book.author}
-            </h3>
-            <p className="availability">
-                {book.isAvailable ? "Available" : "Borrowed"}
-            </p>
-        </div>
+      <div className="metadata">
+        <h2 className="title">{book.title}</h2>
+        <h3 className="author">{book.author}</h3>
+        <p className="availability">
+          {book.isAvailable ? "Available" : "Borrowed"}
+        </p>
+      </div>
     </div>
   );
 }
