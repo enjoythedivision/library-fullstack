327d92b681343ad3afc8cf2bc5e40291fbedb729
enjoythedivision <marialapps@gmail.com>
2026-07-27 10:54:05 +0300

    Install EF Core

---
+++ backend/Data/LibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Data;
+
+public class LibraryContext : DbContext
+{
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
