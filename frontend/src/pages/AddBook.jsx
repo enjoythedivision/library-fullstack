31352dbc70347e28974d72ab5e22f0edb5ba4660
enjoythedivision <marialapps@gmail.com>
2026-07-31 12:45:02 +0300

    Implement AddBook form with submission functionality

--- frontend/src/pages/AddBook.jsx
+++ frontend/src/pages/AddBook.jsx
@@ -1,6 +1,53 @@
 import { useState, useEffect } from "react";
 
 export default function AddBook() {
-    return <>
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const newBook = { title, author, description, isAvailable: true };
+    const response = await fetch(`http://localhost:5038/api/Books`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBook),
+    });
+
+    if (response.ok) {
+      setTitle("");
+      setAuthor("");
+      setDescription("");
+    } else {
+      alert("Failed to add book.");
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          placeholder="Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          value={author}
+          placeholder="Author"
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={description}
+          placeholder="Book description..."
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit">Add Book</button>
+      </form>
     </>
+  );
 }
