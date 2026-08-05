e97e72b7b2747c2dfd07ad4a9b7b1ccc7821d0c8
enjoythedivision <marialapps@gmail.com>
2026-08-05 12:56:15 +0300

    Implement delete functionality in DeleteBook component w/ success alert

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 
 export default function DeleteBook({ books, fetchBooks }) {
   const navigate = useNavigate();
@@ -8,9 +8,28 @@ export default function DeleteBook({ books, fetchBooks }) {
 
   if (!bookToDelete) return <p>Book not found.</p>;
 
+  async function handleDelete(event) {
+    const response = await fetch (`http://localhost:5038/api/Books/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        await fetchBooks();
+        alert("Book deleted successfully.");
+        navigate("/");
+    } else {
+        alert("Failed to delete book.");
+    }
+  }
+
   return (
     <>
-      <p>Are you sure you want to delete {bookToDelete.title} by {bookToDelete.author}?</p>
+      <p>
+        Are you sure you want to delete {bookToDelete.title} by
+        {bookToDelete.author}?
+      </p>
+      <button onClick={handleDelete}>Yes, delete this book</button>
+      <Link to="/">Cancel</Link>
     </>
   );
 }
