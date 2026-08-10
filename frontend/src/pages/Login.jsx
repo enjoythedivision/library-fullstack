1eacb4283c9badbbbf21b541e7fa82fa53a2c3f5
enjoythedivision <marialapps@gmail.com>
2026-08-10 15:04:59 +0300

    Add login form and connect it to Identity API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    const user = {
+      email,
+      password,
+    };
+    const response = await fetch(
+      "http://localhost:5038/login?useCookies=true",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(user),
+      },
+    );
+
+    if (response.ok) {
+      alert("Log in successful!");
+      navigate("/");
+    }
+    else {
+        alert("Failed to login.");
+    }
+  }
+
   return (
     <>
       <h1>Welcome back!</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="mail@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="******"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log in</button>
+      </form>
     </>
   );
 }
