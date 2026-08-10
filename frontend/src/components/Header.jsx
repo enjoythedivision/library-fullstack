912b16b9736cab78405d4867a6a6e67d5d139b9a
enjoythedivision <marialapps@gmail.com>
2026-08-10 20:41:50 +0300

    Add conditional rendering for login button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 
-export default function Header({user}) {
+export default function Header({ user }) {
   return (
     <header>
       <div className="left">
@@ -10,9 +10,21 @@ export default function Header({user}) {
       <div className="right">
         <nav>
           <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link to="/">Logout</Link> 
+                  {/* TODO: Add logout page + logic */}
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
+
             <li>
               <Link to="/">Books</Link>
             </li>
