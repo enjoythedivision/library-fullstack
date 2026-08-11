1bf0b74b2f1ec30a7d7353db32c7dd4edf431e78
enjoythedivision <marialapps@gmail.com>
2026-08-11 19:22:08 +0300

    Update about page and add styling

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,18 +1,22 @@
+import "./About.css"
+
 export default function About() {
   return (
-    <>
       <p>
-        This project is a simple library interface built with React. <br /><br />
-        It allows users to browse, search, and filter books. <br /><br />
-        The goal is not to replicate a full library system (for now!), but to explore core
-        frontend/react concepts like component-based architecture, state management, and
-        dynamic rendering. <br /><br />
-        Since I like taking care of the visual aspect of my projects as well, I implemented a minimal dark interface with vanilla CSS, where each stylesheet lives next to its component.
-        A great alternative option would be SCSS, but due to the small size of the project, I found it redundant. <br /><br />
-        This project is part of an ongoing process of learning and refining web development practices, with future
-        plans to connect it to a backend and evolve it into a fully functional application. <br /><br />
-        Thanks for stopping by!
+        This project is a simple full-stack library application built with React, SQLite
+        and ASP.NET Core. <p>It allows users to browse, search and filter books,
+        as well as create an account and log in to interact with the library.
+        Book data and user accounts are stored in the SQLite database, while
+        ASP.NET Core Identity handles authentication.</p> <p>My goal with this app is to explore
+        core full-stack concepts like component-based architecture, state
+        management, REST APIs, database integration, authentication/authorization, and
+        communication between a React frontend and a .NET backend.</p> <p>Since I like
+        taking care of the visual aspect of my projects as well, I implemented a
+        minimal dark interface with vanilla CSS, where each stylesheet lives
+        next to its component. A great alternative option would be SCSS but due
+        to the small size of the project I found it redundant.</p> <p>The project is
+        still a work in progress as I continue learning and revisiting to better my
+        development practices.</p> <p>Thanks for stopping by! :)</p>
       </p>
-    </>
   );
 }
