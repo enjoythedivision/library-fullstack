ad4e0f0da21d06e1e8c768dfaf4d561886dd3a9e
enjoythedivision <marialapps@gmail.com>
2026-08-11 19:39:32 +0300

    Add BorrowedByUserId column to Books table

---
+++ backend/Migrations/20260811163752_AddBorrowedByUserIdToBook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBorrowedByUserIdToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BorrowedByUserId",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BorrowedByUserId",
+                table: "Books");
+        }
+    }
+}
