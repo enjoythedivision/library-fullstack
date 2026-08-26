5d6633344143d70287e0b98ebc55fb02d29f5501
enjoythedivision <marialapps@gmail.com>
2026-08-26 20:20:20 +0300

    Add user manager to assign Admin role to existing user

--- backend/Program.cs
+++ backend/Program.cs
@@ -39,9 +39,19 @@ using (var scope = app.Services.CreateScope())
     var roleManager = scope.ServiceProvider
         .GetRequiredService<RoleManager<IdentityRole>>();
 
+    var userManager = scope.ServiceProvider
+        .GetRequiredService<UserManager<IdentityUser>>();
+
     if (!await roleManager.RoleExistsAsync("Admin"))
     {
         await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    var adminUser = await userManager.FindByEmailAsync("user@example.com");
+
+    if (adminUser != null)
+    {
+        await userManager.AddToRoleAsync(adminUser, "Admin");
     }
 }
 
